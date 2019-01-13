289c3c2f6bf8dba9ce8c926071586d9c2d7314e1
den <den.gushcha@gmail.com>
2019-01-13 20:47:39 +0300

    i hate to commit something!!!

--- snowfall.py
+++ snowfall.py
@@ -1,6 +1,19 @@
 import pygame
+import random
+from snow import Snow
 
 MAX_X = 1366
 MAX_Y = 768
 MAX_SNOW = 100
 SNOW_SIZE = 64
+
+pygame.init()
+screen = pygame.display.set_mode((MAX_X, MAX_Y), pygame.FULLSCREEN)
+bg_color = (0,0,10)
+
+initialize_snow(MAX_SNOW, SNOW_SIZE)
+
+while True:
+    check_exit()
+    Snow.move_snow()
+    Snow.draw_snow()
